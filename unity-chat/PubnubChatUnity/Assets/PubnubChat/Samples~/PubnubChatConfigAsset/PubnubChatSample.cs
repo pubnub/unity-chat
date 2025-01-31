f2cb0f8e992ece51e36c020aac8ee01d239fd484
PUBNUB\jakub.grzesiowski <jakub.grzesiowski@pubnub.com>
2025-01-31 14:48:36 +0100

    Update unity-side package

--- unity-chat/PubnubChatUnity/Assets/PubnubChat/Samples~/PubnubChatConfigAsset/PubnubChatSample.cs
+++ unity-chat/PubnubChatUnity/Assets/PubnubChat/Samples~/PubnubChatConfigAsset/PubnubChatSample.cs
@@ -14,7 +14,7 @@ public class PubnubChatSample : MonoBehaviour
     private async void Start()
     {
         //Initialize Chat instance with Pubnub keys + user ID
-        var chat = new Chat(configAsset);
+        var chat = await Chat.CreateInstance(configAsset);
 
         //Get config-defined user id handle
         if (!chat.TryGetCurrentUser(out var user))
@@ -24,47 +24,47 @@ public class PubnubChatSample : MonoBehaviour
         }    
 
         //Create a new channel
-        var channel = chat.CreatePublicConversation("MainChannel");
+        var channel = await chat.CreatePublicConversation("MainChannel");
         //Define reaction on receiving new messages
         channel.OnMessageReceived += message => Debug.Log($"Received message: {message.MessageText}");
         //Join channel
-        channel.Join();
+        await channel.Join();
         //Send test message
-        channel.SendText("Hello World from Pubnub!");
+        await channel.SendText("Hello World from Pubnub!");
         
         //React on user data being updated
         user.OnUserUpdated += updatedUser =>
             Debug.Log($"{updatedUser.Id} has been updated! Their name is now {updatedUser.UserName}");
         //Update our user data
-        user.Update(new ChatUserData()
+        await user.Update(new ChatUserData()
         {
             Username = "FancyUserName"
         });
         
         //Send a few more messages
-        channel.SendText("Hi!");
-        channel.SendText("Hello!");
-        channel.SendText("Anyone there?");
-        channel.SendText("Me!");
+        await channel.SendText("Hi!");
+        await channel.SendText("Hello!");
+        await channel.SendText("Anyone there?");
+        await channel.SendText("Me!");
 
         //Wait a moment to wait for them to be processed
         await Task.Delay(15000);
         
         //Fetch message history (from all time)
-        foreach (var historyMessage in channel.GetMessageHistory("99999999999999999", "00000000000000000", 50))
+        foreach (var historyMessage in await channel.GetMessageHistory("99999999999999999", "00000000000000000", 50))
         {
             Debug.Log($"Message from history with timetoken {historyMessage.TimeToken}: {historyMessage.MessageText}");
         }
         
         //Get main users memberships
-        var userMembershipsWrapper = user.GetMemberships();
+        var userMembershipsWrapper = await user.GetMemberships();
         foreach (var userMembership in userMembershipsWrapper.Memberships)
         {
             Debug.Log($"Membership - User: {userMembership.UserId}, Channel: {userMembership.ChannelId}");
         }
         
         //Set a restriction on user
-        user.SetRestriction(channel.Id, new Restriction()
+        await user.SetRestriction(channel.Id, new Restriction()
         {
             Ban = true,
             Mute = true,
@@ -75,7 +75,7 @@ public class PubnubChatSample : MonoBehaviour
         await Task.Delay(15000);
         
         //Print channel's user restriction
-        var restriction = channel.GetUserRestrictions(user);
+        var restriction = await channel.GetUserRestrictions(user);
         Debug.Log($"{user.Id}'s ban status is: {restriction.Ban}, reason: {restriction.Reason}");
     }
 }
